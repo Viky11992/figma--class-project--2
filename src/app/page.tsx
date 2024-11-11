626183428c69e883dac9c2fe559b159822b9bf44
Viky11992 <157172128+Viky11992@users.noreply.github.com>
2024-11-11 19:37:53 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,123 @@
+import React from "react";
+
+function page() {
+  return (
+
+    <div className="h-screen w-screen bg-white relative ">
+      <div className="w-[1440px] h-auto pt-[-590px] pl-[-720px]  bg-[#252B42]">
+        <div className="w-[1322px] h-[91px] ml-[59px] flex gap-[25px]">
+          <div className="w-[187px] h-[58px] mt-[17px] ml-[136px] ">
+            <div className="w-[152px] h-[32px] mt-[13px] ">
+              <h3 className="text-white font-Roboto font-[700] text-[24px] leading-[32px] tracking-[0.1px] capitalize">
+                brandName
+              </h3>
+            </div>
+          </div>
+          <div className="w-[815px] h-[58px] mt-[16px] left-[364px] flex justify-between text-white">
+            <div className="w-[275px] h-[58px] flex item-center gap-[21px] pt-[17px] text-white">
+              <div className="text-[14px] font-Roboto font-[700] leading-[24px] tracking-[0.2px] item-center capitalize">
+                home
+              </div>
+              <div className="text-[14px] font-Roboto font-[700] leading-[24px] tracking-[0.2px] item-center capitalize">
+                product
+              </div>
+              <div className="text-[14px] font-Roboto font-[700] leading-[24px] tracking-[0.2px] item-center capitalize">
+                pricing
+              </div>
+              <div className="text-[14px] font-Roboto font-[700] leading-[24px] tracking-[0.2px] item-center capitalize">
+                contect
+              </div>
+            </div>
+
+            <div className="w-[189px] h-[58px] flex items-center justify-between ">
+              <a className="text-white h-[22px] w-[41px] font-[700] font-Roboto text-[14px] leading-[22px] tracking-[0.2px] items-end capitalize">
+                login
+              </a>
+              <button className="text-white h-[52px] w-[110px] rounded border-r[5px] px-[25px] py-[15px] gap-[15px] bg-[#23A6F0] font-[700] capitalize font-Roboto text-[14px] leading-[22px] tracking-[0.2px]">
+                join us
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="w-[1046px] h-auto text-center  ml-[190px] pt-[80px] pb-[80px] flex flex-col items-center gap-[80px] ">
+          <div className="w-[701px] h-[496px] pt-[40px] pb-[40px] flex flex-col items-center justify-center gap-[40px]  ">
+            <div className="w-[77px] h-[24px]">
+              <h5 className=" text-[16px] font-Roboto font-[700] leading-[24px] tracking-[0.1px] text-[#23A6F0] capitalize">
+                welcome
+              </h5>
+            </div>
+            <div className="w-[542px] h-[160px]  ">
+              <h1 className=" text-[58px] font-Roboto font-[700] leading-[80px] item-center tracking-[0.2px]  text-white">
+                Selling on the internet like a pro
+              </h1>
+            </div>
+            <div className="w-[536px] h-[60px]  ">
+              <h1 className=" text-[22px] font-Roboto font-[400] leading-[30px] item-center tracking-[0.2px]  text-white ">
+                We know how large objects will act, but things on a small scale
+                just do not act that way.
+              </h1>
+            </div>
+            <div className="w-auto h-auto flex gap-[10px]  ">
+              <button className="w-auto h-auto capitalize rounded py-[15px] px-[40px]  bg-[#23A6F0] text-white text-[14px] font-Roboto font-[700] leading-[22px] item-center tracking-[0.2px] ">
+                get quote now
+              </button>
+              <button className="w-auto h-auto capitalize rounded py-[15px] px-[40px]  border border-[#23A6F0] text-[#23A6F0] text-[14px] font-Roboto font-[700] leading-[22px] item-center tracking-[0.2px] ">
+                learn more
+              </button>
+            </div>
+          </div>
+
+          <div className="w-[1046px] h-[292px] text-start flex gap-[30px]">
+            <div className="w-[328px] h-[292px] flex flex-col gap-[20px]  bg-white drop-shadow py-[35px] px-[40px]">
+              <div className="w-[70px] h-[76px] rounded-[10px] bg-[#FFDCD1] py-[22px] px-[19px]"></div>
+              <div className="w-[135px] h-[24px]">
+                <h5 className="text-[16px] font-Roboto font-[700] leading-[24px] tracking-[0.1px] text-[#252B42]">
+                  trainning courses
+                </h5>
+              </div>
+              <div className="w-[50px] h-[2px] bg-[#E74040]"></div>
+              <div className="w-[222px] h-[60px] ">
+                <p className="text-[14px] font-Roboto font-[400] leading-[20px] tracking-[0.2px] text-[#737373]">
+                  The gradual accumulation of information about atomic and
+                  small-scale behaviour...
+                </p>
+              </div>
+            </div>
+            <div className="w-[328px] h-[292px] flex flex-col gap-[20px]  border bg-white drop-shadow py-[35px] px-[40px]">
+              <div className="w-[70px] h-[76px] rounded-[10px] bg-[#B9EAA8] py-[22px] px-[19px]"></div>
+              <div className="w-[168px] h-[24px] ">
+                <h5 className="text-[16px] font-Roboto font-[700] leading-[24px] tracking-[0.1px] text-[#252B42]">
+                  2,769 online courses
+                </h5>
+              </div>
+              <div className="w-[50px] h-[2px] bg-[#E74040]"></div>
+              <div className="w-[222px] h-[60px] ">
+                <p className="text-[14px] font-Roboto font-[400] leading-[20px] tracking-[0.2px] text-[#737373]">
+                  The gradual accumulation of information about atomic and
+                  small-scale behaviour...
+                </p>
+              </div>
+            </div>
+            <div className="w-[328px] h-[292px] flex flex-col gap-[20px]  bg-[#23A6F0] drop-shadow py-[35px] px-[40px]">
+              <div className="w-[70px] h-[76px] rounded-[10px] bg-[white] py-[22px] px-[19px]"></div>
+              <div className="w-[135px] h-[24px]">
+                <h5 className="text-[16px] font-Roboto font-[700] leading-[24px] tracking-[0.1px] text-[white]">
+                  trainning courses
+                </h5>
+              </div>
+              <div className="w-[50px] h-[2px] bg-[white]"></div>
+              <div className="w-[222px] h-[60px] ">
+                <p className="text-[14px] font-Roboto font-[400] leading-[20px] tracking-[0.2px] text-[white]">
+                  The gradual accumulation of information about atomic and
+                  small-scale behaviour...
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default page;
